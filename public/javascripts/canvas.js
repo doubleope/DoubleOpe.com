ecb7d2248b35af5da02c8f476b14db466cc510ab
Opeyemi Openiyi <doubleope@gmail.com>
2019-08-06 11:58:54 -0500

    circles now working

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -7,30 +7,72 @@ var c = canvas.getContext('2d');
 
 
 
-var x = 200;
-var y = 200
-var dx = 4;
-var dy = 4;
-radius = 30;
+
+
+
+function Circle(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+
+    this.draw = function () {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = 'blue';
+        c.stroke();
+    }
+    this.update = function() {
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+    
+        if ( this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+    
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+    }
+}
+
+
+var circleArray = [];
+
+
+for (var i = 0; i < 100; i++) {
+
+    var x = Math.random() * innerWidth;
+    var y = Math.random() * innerHeight;
+    var dx = (Math.random() - 0.5);
+    var dy = (Math.random() - 0.5);
+    radius = 10;
+
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+
+    
+}
+
+console.log(circleArray);
+
+var circle = new Circle(200, 200, 3, 3, 10);
+
+
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
-    c.beginPath();
-    c.arc(x, y, radius, 0, Math.PI * 2, false);
-    c.strokeStyle = 'blue';
-    c.stroke();
+
+    for (var i = 0; i < circleArray.length; i++) {
+        circleArray[i].update();
+    }
+    
+    
     
 
-    if (x + radius > innerWidth || x - radius < 0) {
-        dx = -dx;
-    }
-
-    if ( y + radius > innerHeight || y - radius < 0) {
-        dy = -dy;
-    }
-
-    x += dx;
-    y += dy;
+   
 }
 
 animate();
