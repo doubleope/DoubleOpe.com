630963de922076efbb04d4c7a284edccf9a6d14d
Opeyemi Openiyi <doubleope@gmail.com>
2019-08-07 00:14:46 -0500

    cgg

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+router.get('/cgg', function(req, res, next) {
+  res.render('cgg');
+});
+router.get('/cgg/about', function(req, res, next) {
+  res.render('about');
+});
+
 
 module.exports = router;
